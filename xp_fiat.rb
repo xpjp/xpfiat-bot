932892f7ac88267bd96df10eea03a900481442cb
chipstar <oobayashi@gmail.com>
2017-12-19 15:05:34 +0900

    fix Space missing after colon

--- xp_fiat.rb
+++ xp_fiat.rb
@@ -135,11 +135,11 @@ bot.command :talk_ai do |event, message|
 
   case rand(1..3)
   when 1
-    docomo_talk(event:event, message:message, name:"Xp様", type:"10")
+    docomo_talk(event: event, message: message, name: "Xp様", type: "10")
   when 2
-    docomo_talk(event:event, message:message, name:"浪速のおっちゃん", type:"20")
+    docomo_talk(event: event, message: message, name: "浪速のおっちゃん", type: "20")
   when 3
-    docomo_talk(event:event, message:message, name:"赤さん", type:"30")
+    docomo_talk(event: event, message: message, name: "赤さん", type: "30")
   end
 end
 
@@ -147,7 +147,7 @@ def docomo_talk(event:, message:, name:, type:)
   body = {
     utt: message,
     mode: "dialog",
-    t:type
+    t: type
   }.to_json
   api_key = ENV["DOCOMO_TALK_APIKEY"]
   response = Mechanize.new.post("https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=#{api_key}", body)
@@ -243,7 +243,6 @@ bot.command [:諭吉, :yk] { |event| event.respond "#{event.user.mention} #{say_
 def doge(event)
   d = read_price(:xp_doge)
   amount = 1.0 / d
-  e = event.bot.emoji.select { |e| e.name == 'patty' }
   event.respond "#{event.user.mention} <:doge:391497526278225920>＜ わい一匹で、#{amount.to_i} くらいXPが買えるワン"
 end
 
