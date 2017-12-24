e6531edfa1836dccfe8ac09d0d5b732d90c05f47
toshiki <toshimaru0706@gmail.com>
2017-12-24 15:44:16 +0900

    Remove unintentional spaeces

--- xp_fiat.rb
+++ xp_fiat.rb
@@ -90,7 +90,7 @@ def xp2jpy(event, param1)
     if (amount = param1.to_f).positive?
       _xp_jpy = xp_jpy * amount
       "#{event.user.mention} <:xpchan01:391497596461645824>\
-      ＜ #{amount.to_i.to_s(:delimited)}XPはいま #{_xp_jpy.to_s(:delimited)} 円だよ〜"
+＜ #{amount.to_i.to_s(:delimited)}XPはいま #{_xp_jpy.to_s(:delimited)} 円だよ〜"
     else
       _xp_jpy = xp_jpy.round(8)
       "#{event.user.mention} <:xpchan01:391497596461645824>＜ 1XPはいま #{_xp_jpy.to_s(:delimited)} 円だよ〜"
@@ -105,8 +105,8 @@ bot.command [:xp_jpy, :いくら] { |event, param1| xp2jpy(event, param1) }
 bot.command :どれだけ買える do |event, param1|
   if (yen = param1.to_f).positive?
     amount = yen / xp_jpy
-    event.respond "#{event.user.mention} <:xpchan01:391497596461645824>
-    ＜ #{yen.to_i.to_s(:delimited)}円で #{amount.to_i.to_s(:delimited)}XPくらい買えるよ〜"
+    event.respond "#{event.user.mention} <:xpchan01:391497596461645824>\
+＜ #{yen.to_i.to_s(:delimited)}円で #{amount.to_i.to_s(:delimited)}XPくらい買えるよ〜"
   else
     event.respond "#{event.user.mention} <:xpchan01:391497596461645824>＜ 金額を正しく指定してね〜 :satisfied:"
   end
