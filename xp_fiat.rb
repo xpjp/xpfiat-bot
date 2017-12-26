2f74e1c0d04d9fd6d1b0116531fb7143a8eb396b
soh92 <san4yone3@gmail.com>
2017-12-26 19:59:22 +0900

    出力の小数点以下を４桁に変更

--- xp_fiat.rb
+++ xp_fiat.rb
@@ -50,10 +50,10 @@ def xp2jpy(event, param1)
     if (amount = param1.to_i).positive?
       _xp_jpy = xp_jpy * amount
       "#{event.user.mention} <:xpchan01:391497596461645824>\
-＜ #{amount.to_s(:delimited)}XPはいま #{_xp_jpy.to_s(:delimited)} 円だよ〜"
+＜ #{amount.to_s(:delimited)}XPはいま #{_xp_jpy.round(4).to_s(:delimited)} 円だよ〜"
     else
-      _xp_jpy = xp_jpy.round(8)
-      "#{event.user.mention} <:xpchan01:391497596461645824>＜ 1XPはいま #{_xp_jpy.to_s(:delimited)} 円だよ〜"
+      _xp_jpy = xp_jpy
+      "#{event.user.mention} <:xpchan01:391497596461645824>＜ 1XPはいま #{_xp_jpy.round(4).to_s(:delimited)} 円だよ〜"
     end
   message ||= ":satisfied:"
   event.respond message
