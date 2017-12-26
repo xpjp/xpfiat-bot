a47b3f61b82ef51d91dec401ce76c6c374c4950a
harigel <harisho.0209@gmail.com>
2017-12-26 11:39:13 +0900

    Fix "can't modify frozen String"

--- lib/actions/commands/jpy_xp.rb
+++ lib/actions/commands/jpy_xp.rb
@@ -12,9 +12,9 @@ module Actions
         msg = "#{event.user.mention} <:xpchan01:391497596461645824>"
         if (yen = param1.to_f).positive?
           amount = yen / xp_jpy
-          msg << "＜ #{yen.to_i.to_s(:delimited)}円で #{amount.to_i.to_s(:delimited)}XPくらい買えるよ〜"
+          msg += "＜ #{yen.to_i.to_s(:delimited)}円で #{amount.to_i.to_s(:delimited)}XPくらい買えるよ〜"
         else
-          msg << "＜ 金額を正しく指定してね〜 :satisfied:"
+          msg += "＜ 金額を正しく指定してね〜 :satisfied:"
         end
         event.respond msg
       end
