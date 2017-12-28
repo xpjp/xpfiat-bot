f9aded6fe8850a30888ae43d42e0a0395bbdb081
hir0king <i.hiroki0307@icloud.com>
2017-12-28 21:16:54 +0900

    TalkAIにemotionの機能を統合

--- xp_fiat.rb
+++ xp_fiat.rb
@@ -112,8 +112,7 @@ bc.include_commands Actions::Commands::MakeImg, false # TODO: helpメッセー�
 bc.include_commands Actions::Commands::HowRain, "降雨量の追加(直近100メッセージ)"
 bc.include_commands Actions::Commands::CE, "CoinExhangeのXP/DOGE"
 bc.include_commands Actions::Commands::CM, "CoinsMarketsのXP/DOGE"
-bc.include_commands Actions::Commands::TalkAI, "[message] AIと対話できます"
-bc.include_commands Actions::Commands::TalkAIwithEmotion, "[message] AIと対話(顔付き)"
+bc.include_commands Actions::Commands::TalkAI, "[message] AIと対話できます(表情付きも可)"
 bc.include_commands Actions::Commands::Trend, false
 bc.include_commands Actions::Commands::Translate, false
 
