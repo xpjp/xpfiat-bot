e677113d5d4184e4102c4ecede980dbb05b42c44
Ryo <diaboro77@gmail.com>
2018-01-08 22:25:25 +0900

    [mod] message収集機能実装完了

--- lib/actions/events/subscribe_message.rb
+++ lib/actions/events/subscribe_message.rb
@@ -11,12 +11,17 @@ module Actions
       @subscribe_config = YAML.load_file("./subscribe.yml")
 
       message do |event|
+        p event.channel.id
+        p event.channel.name
+
         # apiの仕様的に存在しないことが保証されているかわからなかったので、落ちないように一応ガード節
-        break if !event.message || !event.channel || !event.user || event.message.content[0] =~ /\?|？/
+        next if !event.message || !event.channel || !event.user || event.message.content[0] =~ /^\?|^？/
         is_target = @subscribe_config["target_channels"].include?(event.channel.name)
 
         body = {
-          sentence: event.message.content
+          sentence: event.message.content,
+          channel_id: event.channel.id,
+          channel: event.channel.name
         }.to_json
         response = Mechanize.new.post("http://198.13.43.77:8080/insert", body )
         json = JSON.parse(response.body)
