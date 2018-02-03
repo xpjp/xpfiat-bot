d9d3a7fb3d66686b49282489d50e3ea585e5a77a
Ryo <diaboro77@gmail.com>
2018-02-04 01:29:01 +0900

    [mod] レビュー後修正

--- lib/actions/events/subscribe_message.rb
+++ lib/actions/events/subscribe_message.rb
@@ -11,24 +11,30 @@ module Actions
       @subscribe_config = YAML.load_file("./subscribe.yml")
 
       message do |event|
-        p event.channel.id
-        p event.channel.name
+        # Debug用
+        # p event.channel.id
+        # p event.channel.name
 
         # apiの仕様的に存在しないことが保証されているかわからなかったので、落ちないように一応ガード節
         next if !event.message || !event.channel || !event.user || event.message.content[0] =~ /^\?|^？/
+        # 取得するチャンネルを選択
         is_target = @subscribe_config["target_channels"].include?(event.channel.name)
+        # 取得対象チャンネル対象以外のとき
+        next unless is_target
 
         body = {
           sentence: event.message.content,
           channel_id: event.channel.id,
           channel: event.channel.name
         }.to_json
+
         # TODO: タイムアウト等の例外対応
-        response = Mechanize.new.post("http://198.13.43.77:8080/insert", body )
-        json = JSON.parse(response.body)
+        Mechanize.new.post("http://198.13.43.77:8080/insert", body )
 
         # Debug用
-        event.respond "#{event.user.mention} #{event.message.content} : #{json['message']}" if is_target
+        # response = Mechanize.new.post("http://198.13.43.77:8080/insert", body )
+        # json = JSON.parse(response.body)
+        # event.respond "#{event.user.mention} #{event.message.content} : #{json['message']}" if is_target
       end
     end
   end
